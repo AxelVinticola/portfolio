369c36c40ea7b771be8cb88749bc916067503d02
Axel Vinticola <axelvinticola@gmail.com>
2026-08-07 01:49:04 -0300

    skill casi temrinadas

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,83 @@
-function Skills(){
+import "../styles/skills.css";
 
-    return(
+import {
+  FaReact,
+  FaPython,
+  FaJava,
+  FaPhp,
+  FaGitAlt,
+  FaGithub,
+  FaDatabase,
+  FaHtml5,
+  FaCss3Alt,
+  FaCode,
+  FaRocket
+} from "react-icons/fa";
 
-        <section id="skills">
+import {
+  SiJavascript,
+  SiDjango,
+  SiMysql,
+  SiFirebase,
+  SiBootstrap,
+} from "react-icons/si";
 
-        </section>
-    )
+function Skills() {
+  return (
+    <section id="skills" className="skills">
 
+      <div className="section-title">
+        <h2>Tecnologías</h2>
+        <p>
+          Estas son las herramientas y tecnologías con las que desarrollo mis
+          proyectos.
+        </p>
+      </div>
+
+      <div className="skills__grid">
+
+        <div className="skill-card">
+          <h3>Frontend</h3>
+
+          <span><FaReact /> React</span>
+          <span><SiJavascript /> JavaScript</span>
+          <span><FaHtml5 /> HTML5</span>
+          <span><FaCss3Alt /> CSS3</span>
+          <span><SiBootstrap /> Bootstrap</span>
+        </div>
+
+        <div className="skill-card">
+          <h3>Backend</h3>
+
+          <span><FaPython /> Python</span>
+          <span><SiDjango /> Django</span>
+          <span><FaJava /> Java</span>
+          <span><FaPhp /> PHP</span>
+          <span>C#</span>
+        </div>
+
+        <div className="skill-card">
+          <h3>Bases de Datos</h3>
+
+          <span><SiMysql /> MySQL</span>
+          <span><SiFirebase /> Firebase</span>
+          <span><FaDatabase /> SQL</span>
+        </div>
+
+        <div className="skill-card">
+          <h3>Herramientas</h3>
+
+          <span><FaGitAlt /> Git</span>
+          <span><FaGithub /> GitHub</span>
+          <span><FaCode /> VS Code</span>
+          <span><FaReact /> React Native</span>
+  <span><FaRocket /> Expo</span>
+        </div>
+
+      </div>
+
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;
